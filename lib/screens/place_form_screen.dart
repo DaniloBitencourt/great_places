bbad3fd117ca8c4d65e391c1af16f8b3dc8212d0
Danilo Bitencourt <danilo300600@gmail.com>
2024-07-17 19:37:46 -0300

    Criando a estrutura do Formulário da tela de place_form

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/image_input.dart';
+
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({super.key});
 
@@ -8,14 +10,54 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  
+  final _titleController = TextEditingController();
+
+  void _submitForm(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Novo Lugar'),
       ),
-      body: Center(
-        child: Text('Form!!!'),
+      body: Column(
+        
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            label: Text('Adicionar'),
+            icon: Icon(Icons.add),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Theme.of(context).colorScheme.onSecondary, 
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            
+          )
+        ],
       ),
     );
   }
